4bb9894b0a4f5ccf465ae86471d985aaba08d71f
ValeriyKov <valeriy.kov@hotmail.com>
2019-04-10 17:26:49 +0500

    task complited

--- LongestCommonSubsequenceCalculator.cs
+++ LongestCommonSubsequenceCalculator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Antiplagiarism
 {
@@ -13,12 +15,43 @@ namespace Antiplagiarism
 
         private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
         {
-            throw new NotImplementedException();
+            var opt = new int[first.Count + 1, second.Count + 1];
+         
+            for (var i = 1; i < first.Count + 1; i++)
+                for (var j = 1; j < second.Count + 1; j++)
+                {
+                    if (first.Count == 0 || second.Count == 0)
+                        opt[i, j] = 0;
+                    if (first[i - 1] == second[j - 1])
+                        opt[i, j] = opt[i - 1, j - 1] + 1;
+                    else
+                        opt[i, j] = Math.Max(opt[i - 1, j], opt[i, j - 1]);
+                }
+
+            return opt;
         }
 
         private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+            var i = first.Count;
+            var j = second.Count;
+            while (i > 0 && j > 0)
+            {
+                if (first[i - 1] == second[j - 1])
+                {
+                    result.Add(first[i - 1]);
+                    i -= 1;
+                    j -= 1;
+                }
+                else if (opt[i - 1,j] == opt[i,j])
+                    i -= 1;
+                else
+                    j -= 1;
+            }
+
+            result.Reverse();
+            return result;
         }
     }
 }
