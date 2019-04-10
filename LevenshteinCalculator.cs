096301e49468bcea7cdf15c505ed4a1db8ff4658
ValeriyKov <valeriy.kov@hotmail.com>
2019-04-10 10:17:23 +0500

    part 1 - Ok

--- LevenshteinCalculator.cs
+++ LevenshteinCalculator.cs
@@ -29,55 +29,30 @@ namespace Antiplagiarism
             if (a.Count == 0) return b.Count;
             if (b.Count == 0) return a.Count;
 
-            var max = Math.Max(a.Count, b.Count);
-            var  distances = new Double[2, max + 1];
-           
-            for (var j = 0;  j <= max;  distances[0, j] = j++);          
+            var distances = new double[2, b.Count + 1];
+            for (var i = 0;  i <= b.Count;  distances[0, i] = i++);
+            distances[1, 0] = 1;
             
             for (var i = 1;  i <= a.Count;  i++)
+            {
                 for (var j = 1;  j <= b.Count;  j++)
                 {
                     var replace = TokenDistanceCalculator.GetTokenDistance(a[i - 1], b[j - 1]);
                     var replaceCost = b[j - 1] == a[i - 1] ? 0 : replace;
-                    
+                   
                     distances[1,j] = Math.Min
                     (
                         Math.Min(distances[0, j] + 1, distances[1, j - 1] + 1),
                         distances[0, j - 1] + replaceCost
                     );
                 }
-            
-            var temp = distances[0, j];
-            distances[0, j] = distances[1, j];
-            distances[1, j] = temp;
-            
-            return distances[1, b.Count];
-        }
-        
-        private Double CalcLevenshteinDistanceBefore(List<string> a, List<string> b)
-        {
-            if (a.Count == 0 && b.Count == 0) return 0;
-            if (a.Count == 0) return b.Count;
-            if (b.Count == 0) return a.Count;
-            
-            var  distances = new Double[a.Count + 1, b.Count + 1];
-           
-            for (var i = 0;  i <= a.Count;  distances[i, 0] = i++);
-            for (var j = 0;  j <= b.Count;  distances[0, j] = j++);          
-            
-            for (var i = 1;  i <= a.Count;  i++)
-            for (var j = 1;  j <= b.Count;  j++)
-            {
-                var change = TokenDistanceCalculator.GetTokenDistance(a[i - 1], b[j - 1]);
-                var cost = b[j - 1] == a[i - 1] ? 0 : change;
-                    
-                distances[i,j] = Math.Min
-                (
-                    Math.Min(distances[i - 1, j] + 1, distances[i, j - 1] + 1),
-                    distances[i - 1, j - 1] + cost
-                );
+
+                for (var k = 0; k < distances.GetLength(1); k++)
+                    distances[0,k] = distances[1,k];
+                distances[1, 0]++;
             }
-            return distances[a.Count, b.Count];
+            
+            return distances[0, b.Count];
         }
     }
 }
